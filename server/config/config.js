7672bc3432aac9d9f301fbbd882592c81a1cd81a
David Uribe <daviduribe1997@gmail.com>
2020-03-25 17:30:16 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,29 @@
 
 
-// ===================
-//      PUERTO
-// ===================
+// =============================
+//              PUERTO
+// =============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// =============================
+//              ENTORNO
+// =============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =============================
+//        BASE DE DATOS
+// =============================
+
+//let dbUser = encodeURIComponent('dbdu');
+//let dbPassword = encodeURIComponent('qcNDRJCZWNC3cxe6');
+let urlDB = null;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = `mongodb+srv://dbdu:qcNDRJCZWNC3cxe6@cafe-tydtb.mongodb.net/test?retryWrites=true&w=majority`;
+}
+
+process.env.URLDB = urlDB;
