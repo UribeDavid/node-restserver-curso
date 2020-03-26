e906f5f42a8142d0b4133bd38784bde189191246
David Uribe <daviduribe1997@gmail.com>
2020-03-25 19:25:35 -0500

    No sé que pasa

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect(process.env.URLDB, (err, res) => {
+mongoose.connect(process.env.URLDB, {useUnifiedTopology: true, useNewUrlParser: true}, (err, res) => {
 
     if ( err ) throw err;
 
